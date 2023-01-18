c6aaea390301807e6a1d8131ea88301e922248d1
thiyangt <thiyanga.talagala@monash.edu>
2023-01-18 16:22:28 +0530

    restructuring the package: added all url

--- data-raw/1_level_of_risk.R
+++ data-raw/1_level_of_risk.R
@@ -6,32 +6,31 @@ library(rvest)
 library(tidyverse)
 library(magrittr)
 
-
-url <- "https://www.cdc.gov/dengue/areaswithrisk/around-the-world.html"
-data <- read_html(url) 
-data
-
-## Africa
-africa_selector <- "body > div.container.d-flex.flex-wrap.body-wrapper.bg-white > main > div:nth-child(3) > div > div:nth-child(4) > div:nth-child(3) > div > div:nth-child(5) > table"
-africa <- url %>% 
-  read_html() %>% 
-  html_element(css = africa_selector) %>%
-  html_table()
-dim(africa)
-africa$Region <- rep("Africa", dim(africa)[1])
-head(africa)
-
-## Asia
-
-asia_selector <- "body > div.container.d-flex.flex-wrap.body-wrapper.bg-white > main > div:nth-child(3) > div > div:nth-child(4) > div:nth-child(3) > div > div:nth-child(8)"
-asia <- url %>% 
-  read_html() %>% 
-  html_element(css = asia_selector) %>%
-  html_table()
-dim(asia)
-asia$Region <- rep("Asia", dim(asia)[1])
-head(asia)
-
+## General function
 ## General function
 
-read_data_web <- functi
+read_data_web <- function(url, selector){
+  url %>% 
+    read_html() %>% 
+    html_element(css = selector) %>%
+    html_table()
+  
+}
+
+## Read data
+
+url <- "https://www.cdc.gov/dengue/areaswithrisk/around-the-world.html"
+## Africa
+africa_selector <- "body > div.container.d-flex.flex-wrap.body-wrapper.bg-white > main > div:nth-child(3) > div > div:nth-child(4) > div:nth-child(3) > div > div:nth-child(5) > table"
+## Asia
+asia_selector <- "body > div.container.d-flex.flex-wrap.body-wrapper.bg-white > main > div:nth-child(3) > div > div:nth-child(4) > div:nth-child(3) > div > div:nth-child(8)"
+## Europe 
+europe_selector <- "body > div.container.d-flex.flex-wrap.body-wrapper.bg-white > main > div:nth-child(3) > div > div:nth-child(4) > div:nth-child(3) > div > div:nth-child(11) > table > tbody"
+## Americas
+america_selector <- "body > div.container.d-flex.flex-wrap.body-wrapper.bg-white > main > div:nth-child(3) > div > div:nth-child(4) > div:nth-child(3) > div > div:nth-child(13)"
+## Oceania/Pacific Islands
+oceania_selector <- ""
+
+
+
+
