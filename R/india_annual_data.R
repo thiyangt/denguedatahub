b94d03d75ac2d376958be12c741348856c49c034
thiyangt <thiyanga.talagala@monash.edu>
2023-01-30 12:16:08 +0530

    completed india files

--- R/india_annual_data.R
+++ R/india_annual_data.R
@@ -1,10 +1,10 @@
 #' DENGUE/DHF situation in India since 2017
-#' @description State/Union Territory\(UT\)-wise dengue/DHF annual deaths and cases since 2017 (Last accessed from the source on 30 January 2023). 
+#' @description State/Union Territory(UT)-wise dengue/DHF annual deaths and cases since 2017 (Last accessed from the source on 30 January 2023). 
 #' @format A tibble with 432 rows and 5 variables:
 #' \describe{
-#'   \item{Affected States/UTs}{ Name of the affected states/UTs}
-#'   \item{type}{ An indicator for the type of cases (deaths, cases)} 
-#'   \item{year}{ Year}
+#'   \item{Affected States/UTs}{Name of the affected states/UTs}
+#'   \item{type}{An indicator for the type of cases (deaths, cases)} 
+#'   \item{year}{Year}
 #'   \item{additional_information}{Additional information regarding collected year period}
 #'   \item{value}{Cases}
 #'    }
