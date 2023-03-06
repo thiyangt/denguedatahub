69c3b4cdc9bd8cb5279087236122f475f41f6c27
thiyangt <thiyanga.talagala@monash.edu>
2023-03-06 17:40:29 +0530

    updared dengue datahub

---
+++ R/srilanka_weekly_data.R
@@ -0,0 +1,15 @@
+#' Weekly number of dengue fever infections in Sri Lanka
+#' @description Weekly cases of dengue in Sri Lanka
+#' @format A tibble with 18772 rows and 6 variables:
+#' \describe{
+#'   \item{year}{year}
+#'   \item{week}{week number} 
+#'   \item{start.date}{starting date of the week}
+#'   \item{end.date}{ending date of the week}
+#'   \item{division}{district name}
+#'   \item{cases}{Number of dengue cases}
+#'    }
+#' @source \url{https://ourworldindata.org/grapher/dengue-incidence}
+#' @examples 
+#' head(srilanka_weekly_data)
+"srilanka_weekly_data"
---
+++ data-raw/sri_lanka.R
@@ -0,0 +1,19 @@
+# source: extract dengue counts from the DISC package
+library(here)
+library(tibble)
+load(here("data-raw", "Data_Counts.rda"))
+sl <- Data_Counts
+year <- sl$Year
+week <- sl$Week
+start.date <- sl$StartDate
+end.date <- sl$EndDate
+division <- sl$Division
+dengue.cases <- sl$Dengue
+srilanka_weekly_data <- tibble(year = year,
+                               week = week,
+                               start.date = start.date,
+                               end.date = end.date,
+                               district = division,
+                               count = dengue.cases
+                               )
+save(srilanka_weekly_data, file=here("data", "srilanka_weekly_data.rda"))
