3df2f71467db23fc0f30208836ed48a461d0a7b8
thiyangt <thiyanga.talagala@monash.edu>
2023-03-10 07:56:20 +0530

    updated

--- data-raw/data_sources.R
+++ data-raw/data_sources.R
@@ -1,3 +1,6 @@
 #https://data.cdc.gov/browse.php?federation_filter=85&format=php&limitTo=datasets&sortBy=alpha&tags=dengue
 
 #https://data.cdc.gov/browse.php?federation_filter=85&format=php&sortBy=alpha&tags=dengue
+
+# map
+#https://www.healthmap.org/dengue/en/
