258cc46c3ddb7b35612a269b732739c0979e3a92
thiyangt <thiyanga.talagala@monash.edu>
2023-03-10 07:18:06 +0530

    updated world annual file

--- R/world_annual.R
+++ R/world_annual.R
@@ -2,12 +2,12 @@
 #' @description Annual incidence of dengue around the world
 #' @format A tibble with 6750 rows and 4 variables:
 #' \describe{
-#'   \item{Entity}{Country or area name}
-#'   \item{Code}{Country or area code} 
-#'   \item{Year}{year}
-#'   \item{Incidence}{Number of dengue incidence across all ages}
+#'   \item{entity}{Country or area name}
+#'   \item{code}{Country or area code} 
+#'   \item{year}{year}
+#'   \item{incidence}{Number of dengue incidence across all ages}
 #'    }
 #' @source \url{https://ourworldindata.org/grapher/dengue-incidence}
 #' @examples 
-#' head(world.annual)
-"world.annual"
+#' head(world_annual)
+"world_annual"
--- data-raw/our_world_data.R
+++ data-raw/our_world_data.R
@@ -1,12 +1,12 @@
 # source: https://ourworldindata.org/grapher/dengue-incidence
-# lase accesses: 20 Feb 2023
+# lase accesses: 10 March 2023
 
 library(readr)
 library(here)
-world.annual <- read_csv(here("data-raw", "dengue-incidence.csv"))
-head(world.annual)
-dim(world.annual)
-colnames(world.annual)
-View(world.annual)
-colnames(world.annual) <- c("Entity", "Code", "Year", "Incidence")
-save(world.annual, file=here("data", "world.annual.rda"))
+world_annual <- read_csv(here("data-raw", "dengue-incidence.csv"))
+head(world_annual)
+dim(world_annual)
+colnames(world_annual)
+View(world_annual)
+colnames(world_annual) <- c("entity", "code", "year", "incidence")
+save(world_annual, file=here("data", "world_annual.rda"))
