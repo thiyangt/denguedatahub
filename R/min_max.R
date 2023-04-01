f1e170073ea3bad8dca015e746954b3e8bca9c30
thiyangt <thiyanga.talagala@monash.edu>
2023-04-01 09:34:17 +0530

    update the function

--- R/min_max.R
+++ R/min_max.R
@@ -1,11 +1,21 @@
 #' Apply min-max transformation
 #' @param 
-min_max <- function(data, variable, local=FALSE, key){
+min_max <- function(data, variable.to.minmax, local=FALSE, key){
   
   if (local=FALSE){
-    
+    vtm <- eval(pars$data)[, as.numeric(pars$variable.to.minmax)]
+    range.vtm <- range(vtm)
+    data$min.max <- (vtm - range.vtm[1])(range.vtm[2] - range.vtm[1])
+    tbl <- as_tibble(data)
   } else{
+    
+    tbl <- pars$data %>% 
+      group_by(pars$key) %>%
+      summarize(min = ,
+                max = ,
+                min.max = )
     
   }
   
+  tbl
 }
