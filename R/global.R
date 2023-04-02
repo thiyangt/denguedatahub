59113440746e0e2326c0f9019b12c33a50ed4776
thiyangt <thiyanga.talagala@monash.edu>
2023-04-02 15:05:30 +0530

    updated minmax function

--- R/global.R
+++ R/global.R
@@ -1,4 +1,4 @@
 utils::globalVariables(c("x", "map_dfr", "district",
                          "cases",
-                         "ad.list", "y"
+                         "ad.list", "y", "key"
                          ))
--- R/min_max.R
+++ R/min_max.R
@@ -6,33 +6,41 @@
 #' @export
 min_max <- function(data, variable.to.minmax, local=FALSE, group.var){
   
-  if (local=FALSE){
+  if (local==FALSE) {
     
-    range.vtm <- range(variable.to.minmax)
-    data$min.max <- (variable.to.minmax - range.vtm[1])(range.vtm[2] - range.vtm[1])
-    tbl <- as_tibble(data)
-    
-  } else{
-    
-    group_var <- enquo(key)
     cases = as.name(variable.to.minmax)
     
-    data %>% dplyr::group_by(!!group_var) %>% 
+    data.tbl <- data %>% 
+      mutate(
+        min.cases = min(!!enquo(cases)),
+        max.cases = max(!!enquo(cases)),
+        minmax.cases = (!!enquo(cases) - min(!!enquo(cases)))/(max(!!enquo(cases)) - min(!!enquo(cases)))
+      )
+    
+  } else {
+    
+    group_var <- enquo(group.var)
+    cases = as.name(variable.to.minmax)
+    
+    data.tbl <- data %>% dplyr::group_by(!!group_var) %>% 
       mutate(
         min.group = min(!!enquo(cases)),
         max.group = max(!!enquo(cases)),
         minmax.group = (!!enquo(cases) - min(!!enquo(cases)))/(max(!!enquo(cases)) - min(!!enquo(cases)))
+      
       )
-    
-  }
+    }
   
-  data
+  data.tbl
 }
-@example
+
+#'@example
 #' library(tibble)
 #' value <- c(rnorm(20, mean=0), 
 #'           rnorm(20, mean=20))
 #' key <- c(rep(1, 20), rep(2, 20))
 #' tbl <- tibble(values=value, keys=key)
-#'min_max(tbl, "values", local=TRUE, "keys")
-#'min_max(tbl, "values", "keys")
+#'ex1 <- min_max(tbl, "values", local=TRUE, "keys")
+#'ex1
+#'ex2 <- min_max(tbl, "values",local=FALSE, "keys")
+#'ex2
